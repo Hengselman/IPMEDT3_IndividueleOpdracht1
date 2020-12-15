dfec2d5dd54caa13066e30e4962d930c9c31fab9
Hengselman <Hengselman@Gmail.com>
2020-12-15 16:46:14 +0100

    Mengpaneel toegevoegd

--- main.js
+++ main.js
@@ -6,11 +6,51 @@ window.onload = () => {
   const camera = document.getElementById('js--camera');
   const cursor = document.getElementById('js--entityCursor')
 
+  const mengbal1 = document.getElementsByClassName('js--mengbal1');
+  const mengbal2 = document.getElementsByClassName('js--mengbal2');
+  const mengplaat1 = document.getElementById('js--mengplaat1');
+  const mengbox1 = document.getElementById('js--mengbox1');
+  const mengplaat2 = document.getElementById('js--mengplaat2');
+  const mengbox2 = document.getElementById('js--mengbox2');
+  const gemengd = document.getElementById('js--gemengd');
+
+
+
   let brush = "grey";
+  var gekozenKleur1;
+  var gekozenKleur2;
+
+  for(let i=0; i < mengbal1.length; i++){
+    mengbal1[i].onmouseenter = (event) => {
+      gekozenKleur1 = mengbal1[i].getAttribute("color");
+      mengplaat1.setAttribute("color", gekozenKleur1);
+      mengbox1.setAttribute("color", gekozenKleur1);
+      mengKleuren();
+    }
+  }
+
+  for(let i=0; i < mengbal2.length; i++){
+    mengbal2[i].onmouseenter = (event) => {
+      gekozenKleur2 = mengbal2[i].getAttribute("color");
+      mengplaat2.setAttribute("color", gekozenKleur2);
+      mengbox2.setAttribute("color", gekozenKleur2);
+      mengKleuren();
+      }
+    }
+
+  function mengKleuren() {
+    console.log("Start functie");
+    if(gekozenKleur1 == gekozenKleur2){
+      console.log("Dezelfde kleur");
+      gemengd.setAttribute("color", gekozenKleur1);
+  } else if (gekozenKleur1 == 'red' && gekozenKleur2 == 'blue') {
+      gemengd.setAttribute("color", "purple");
+    }
+  }
+
 
   for(let i=0; i < planes.length; i++){
     planes[i].onmouseenter = (event) => {
-      console.log(brush);
       planes[i].setAttribute("color",brush);
     }
   }
@@ -18,10 +58,10 @@ window.onload = () => {
   for(let i=0; i < spheres.length; i++){
     spheres[i].onmouseenter = (event) => {
       brush = spheres[i].getAttribute("color");
-      console.log(brush);
       cursor.setAttribute("material", "color", brush);
     }
   }
+
 
   for (let i = 0; i < places.length; i++) {
     places[i].addEventListener('click', function(evt){
